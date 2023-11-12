3f60326490a8f9062541b8204acad19fd0900638
Kyle Stultz <kylostultz@gmail.com>
2023-11-11 19:24:44 -0500

    Exercise 1 and 2 complete

---
+++ app.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    var searchbutton = document.getElementById("btn")
+    searchbutton.addEventListener ('click',function(e) {
+        e.preventDefault();
+        var searchquery = document.getElementById("superheroes").value;
+        var herorequest = new XMLHttpRequest();
+        var urlcode = "superheroes.php?query=" + searchquery;
+        hrequest.onreadystatechange = function() {
+            if (herorequest.readyState == XMLHttpRequest.DONE){
+                if (herorequest.status == 200) {
+                    var hero = herorequest.responseText;
+                    var result = document.getElementById("result");
+                    result.innerHTML = hero;
+                } else{
+                    alert("Error Detected");
+                }
+            }
+        }
+        hrequest.open("GET" , urlcode, true);
+        hrequest.send();
+
+    });
+}
